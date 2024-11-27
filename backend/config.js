de6c9334ce0db2e06b3602121cf5475f9f82668c
Homeshika <Sharmahomeshika@gmail.com>
2024-11-27 12:13:17 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://wrongperson875:2UXnVIk79Hwh0VwQ@cluster0.rkgs3vw.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
